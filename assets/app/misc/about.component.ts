2442fbc246b69a4320f4737417edbca977734178
AaronBP <aaron.phillips96@icloud.com>
2016-07-02 17:08:19 +0100

    Add about page template

--- assets/app/misc/about.component.ts
+++ assets/app/misc/about.component.ts
@@ -2,9 +2,80 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-about',
-    template: 'about page'
+    template: `
+<div class="page-header">
+    <h2>About Me</h2>
+</div>
+<!--Keep headers in divs with class title. -->
+
+<div>
+    I'm dedicating this page as a place to store the following information:
+    <ul>
+        <li>A summary of my life, </li>
+        <li>Where the inspiration to build TheManWhoCan came from,</li>
+        <li>What to bare in mind when reading articles on TheManWhoCan.</li>
+    </ul>
+</div>
+
+<div class="heading">My Story</div>
+<p>I was hatched and raised in St Ives, UK - a small town near Cambridge. Quite early on through school I discovered the right side of my brain from learning guitar, the first (of many) things I started to teach myself. I loved the freedom it created in my life, but couldn't tame it (and still haven't managed to)! I find when self-teaching a skill and not following a curriculum it is very easy to lose site of what it is you're trying to achieve... Anyway, I digress.</p>
+
+<p>So as I pass through the schooling system neither loving nor hating it I find myself asking the question we all face: “This is all great, but what the hell am I going to do when I leave education?!” So, so many different answers to this question have crossed my mind – how does one fully decide as a teenager what they want to do until retirement?</p>
+
+<p>It's a tough question. I had no idea what the right answer would be and when I had an idea I would question it 5 hours a day, making sure I was making the right decision, ultimately leading to me being put off by the idea!</p>
+
+<p>As the time of GCSEs and A-Levels approached I started going through the phase of having girlfriends, not to blame them, but my focus wasn't solely on myself or what direction my life was heading in. I would spend my days thinking of nothing productive, causing arguments with those around me. I'd do work at school, but my heart wasn't in it.</p>
+
+<p>Results day for the first year of my A-Levels approached, I had failed Chemistry. To cut a long story short: this resulted in me not being able to continue on with A-Levels as others in my year had. Unsurprisingly, the fact I had failed did not bother me; I didn't work hard, I didn't deserve to pass. I followed the crowd of others who had failed their first year and resat, which involved a year identical to the first. One thing being different. CUE THE SINGLE STREAK. </p>
+
+<p>Yeah..... No more relationships for a long time!</p>
+
+<p>Partly because nobody of interest (in that way) showed, partly because when they did they weren't interested! I used this time wisely however, as I happened to come across a man by the name of Ralph Smart on YouTube who speaks about becoming the best version of yourself. </p>
+
+<p>My life was transformed. </p>
+
+<p>I started noticing more about my personality, started working with it to ease out the anger and frustration in my life. I started meditating, started to work harder at what I enjoyed doing, started to focus on myself and nothing else.</p>
+
+<p>This... This was a major turning point in my life.</p>
+
+<p>I was really enjoying mathematics at this point in my life and decided it was my life purpose. This was a bold statement. In my earlier years I would have never even thought about loving maths (Americans – get off my back!) as much as did at this point in my life. Then came the results of the year of my resit: A, B, C in Maths Biology and Physics respectively. I was over the moon! The happiest I'd ever been about anything I'd ever achieved in my life. I decided to drop biology and teach myself further maths, since revision for biology seemed to take up all my time and the teachers bored me (lets hope they don't find out I blog and hold an even bigger grudge against me)! </p>
+
+<p>I started to apply for university hoping to eventually obtain a degree in Chaos Theory, a topic which I read about in my spare time and found fascinating. I applied for 5 universities to study MMath (an undergraduate degree that continues a year longer than a bachelors) which would ultimately allow me to study for a PhD afterwards. I received offers from all 5 universities! My personal high was on course to meet to peak of Mount Everest!</p>
+
+<p>Spoiler alert, my life takes a somewhat unexpected turn (or not so unexpected)!</p>
+
+<p>As you might have noticed early on in this story, I question myself a lot! Sometimes this is great, sometimes it's not. It's hard to live a life where you're unsure if you're happy or not, on the other hand it's lovely when you make it through the questioning phase and you're sure that that thing you were questioning had passed the test. </p>
+
+<p>In this case, mathematics failed.</p>
+
+<p>I started getting stressed from learning so much of it and trying to fulfil my teachers expectations of me to do well. But worst of all it was me who expected me to do well. </p>
+<p>I'd make a mathematical concept my life until I understood it because if I didn't I wouldn't be my best. Filling my day with nothing but equations lead to my brain being numb 80% of the time and caused unhappiness and many headaches. </p>
+
+<p>It put me off. I was so obsessed that I became... “unobsessed”. I realise now that just because you think something is your life purpose doesn't mean your life has to revolve around it and that stopping is just as - if not more beneficial – than carrying on if you're chasing a dream. Time to reflect is just as important, balancing the two is something I'm still working on. </p>
+
+<p>Somewhere along the way, I started teaching myself coding online (another of the many things I've taught myself). My life slowly started orientating to me finding myself at my laptop doing small projects and building anything and everything I could. My headaches didn't persist long at all when I was coding. My mathematical rehabilitation was filled with lines of code!</p>
+
+<p>I decided not to follow through with going to university, and instead become a web developer. I took a course on Udemy about front and back end development and completed the course in a very small time scale whilst still studying for my A-Levels (although now studying for my A-Levels consisted of me bunking the majority of the week and spending my time on my laptop building websites). </p>
+
+<p>At present, it is a Sunday before before my final A-Level exams start. I plan on doing revision, but not so much that I get a headache. I plan on trying my hardest, without making myself unhappy. I plan on doing as much web development as I have time for, as it uplifts me and inspires me to be my best.</p>
+
+<p>I think the reason for this love of web development would be down to the connection of my right brain from my childhood (guitar) and my left from my later years (maths). It is a constant reminder of who I have been, the change in course my life has taken and everything in between. Which I find quite poetic.</p>
+
+<p>This sudden change of heart between mathematics and web development does not mean that I now hate maths and never want to do it again. I'm extremely grateful that maths was there as a safety net, helping me turn my life around. For this reason it will always be that friend from the past. Since now we've gone our separate ways, a limited amount of time together would probably be for the best!</p>
+
+<div class="heading">What Inspired Me To Build TheManWhoCan</div>
+
+<p>The twists and turns of life can leave you confused, restless and stuck. I've found it along my journey. My change of heart is what inspired me to help others get through these times whilst making the decisions that best suits them. </p>
+
+<div class="heading">Bare This In Mind</div>
+
+<p>I am just a boy! I do not hold the answers to your life. Dammit, I don't even hold the answers to my life! I can only help awaken you to be clear headed when the time to make decisions approaches.</p>
+
+<p>Also bare in mind that when I refer to my experiences I understand that people might have gone through much tougher and more devastating circumstances. I do not claim to be able to make it out the other side of any situation I have not yet been through.</p>
+    
+    `
 })
 
 export class AboutComponent{
-    
+
 }
